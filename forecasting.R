8da2b0d49b517dbf03a84867915f9face16f3f66
Rodrigo Gabino Ramírez Moreno <jadwer@msn.com>
2019-04-08 06:26:47 -0500

    Project files 2

--- forecasting.R
+++ forecasting.R
@@ -1,68 +1,84 @@
+library(dplyr)
 library(forecast)
+library(reshape2)
 library(readxl)
-library(urca)
-library(vars)
-library(tibble)
 
-var.data<-read_excel('/Users/JwR-MBP15/Desktop/R/Pronosticos con R/Datos/Datos Ultimas Clase.xlsx', sheet = 'FULL')
-var.data$Fecha<-as.Date(var.data$Fecha)
+datos<-read_excel('/Users/JwR-MBP15/Desktop/Transgenia/devs/e-model/data/timeline.xlsx', sheet = 'FULL')
+datos$Fecha<-as.Date(datos$Fecha)
 
-colnames(var.data)
-no_diff<-c("CETES28", "DGS1M")
+#datos<-subset(datos, Fecha<=)
+colnames(datos)
+quitar<-c("Subyacente1", "Subyacente2", "Subyacete", "Nsubyacente","Nsubyacente1","Nsubyacente2")
 
-var.data<-var.data %>% subset(Fecha<=as.Date("2019-02-01") & Fecha>=as.Date("2001-07-01"))
+datos<-datos %>% select(-one_of(quitar)) %>% subset(Fecha<=as.Date("2019-02-01") & Fecha>=as.Date("2001-07-01"))
+datos_scaled<-datos
 
-ts.data<-ts(var.data[,-1], start = c(2001, 07), frequency = 12)
-ts.tasas<-ts.data[, no_diff]
-x1<-colnames(var.data)[-1][!(colnames(var.data)[-1] %in%  no_diff)]
-ts.notasas<-diff(log(ts.data), 12)[,x1]
-ts.notasas<-ts.notasas*100
+maxs <- apply(datos_scaled[,-1], 2, max, na.rm=T) 
+mins <- apply(datos_scaled[,-1], 2, min, na.rm=T)
+scaled<-scale(datos_scaled[, -1], center = mins, scale = maxs - mins)
 
+datos_scaled[, -1]<-scaled
 
-ts.var<-ts.union(ts.tasas, ts.notasas)
-colnames(ts.var)<-gsub('ts.tasas.', '',  colnames(ts.var))
-colnames(ts.var)<-gsub('ts.notasas.', '',  colnames(ts.var))
-
-sample.ts<- window(ts.var,start=c(2002,7), end=c(2018, 1))
-sample.ts.2<-window(ts.var, start=c(2018, 2), end=c(2018,12))
-
-#Reproducción del paper de Espadas
-#Estimación del modelo VAR
-INPCS<-c("INPC","Subyacente1", "Subyacente2", "Subyacete", 
-         "Nsubyacente","Nsubyacente1","Nsubyacente2")
-
-exogenas<-c( 'INDPRO','DGS1M', 'CPIAUCSL', 'COMM')
-endogenas<-c('IGAE_TRC', "CETES28" ,'USDMXN')
-
-ts.exogenas<-sample.ts[, exogenas]
-
-all.vars.inpc<-lapply(1:length(INPCS), function(ipc){
-
-  index.prices<-INPCS[ipc]
-  endogenas<-c(endogenas, index.prices)
-  ts.endogenas<-sample.ts[, endogenas]
-  
-  selection<-VARselect(ts.endogenas, lag.max = 6, type = c("const"),exogen = ts.exogenas)
-  order.var<-selection$selection[1]
-  fit.var<-VAR(ts.endogenas, p=order.var, exogen = ts.exogenas, type = 'const')
-  impulse.response<-irf(fit.var, impulse='USDMXN', response=index.prices, boot=T)
-  
-  resume<-tibble(INPC=index.prices, VAR=list(fit.var), IR=impulse.response)
-  return(resume)  
-  
-  })
-
-all.vars.inpc<-do.call('rbind', all.vars.inpc)
-
-ts.exogenas.h<-sample.ts.2[, exogenas]
-var.forecast<-predict(all.vars.inpc$VAR[[1]], dumvar=ts.exogenas.h, n.ahead = nrow(ts.exogenas.h))
-target.var<-var.forecast$fcst[[INPCS[1]]]
-
-pronosticado=ts(target.var[,1], start=start(sample.ts.2), frequency = 12)
-observado=sample.ts.2[, INPCS[1]]
-
-vars.backtesting<-ts.union(pronosticado, observado)
-forecast::autoplot(vars.backtesting)
+autoplot(ts.data)
 
 
+ts.data<-ts(datos_scaled[,-1], start = c(2001, 07), frequency = 12)
+ts.data<-diff(ts.data, 12)
 
+sample.ts<- window(ts.data, end=c(2018, 1))
+sample.ts.2<-window(ts.data, start=c(2018, 2), end=c(2018,12))
+
+winners_step<-step(lm(INPC~., data = sample.ts), trace = 0) %>% coef() %>% names()
+winners_step[-1]
+exogena<-winners_step[-1]
+
+
+target<-'INPC'
+h=nrow(sample.ts.2)
+# Linear ------------------------------------------------------------------
+
+exogenas<-paste0(exogena, collapse='+')
+modelo_ts<-as.formula(paste0(target ,'~',exogenas ))
+
+ts_SPX<-tslm(modelo_ts, data = sample.ts)
+ts_f_spx<-forecast(ts_SPX, newdata = data.frame(sample.ts.2))
+
+# Univariantes ------------------------------------------------------------
+
+SPX_ARIMA<-auto.arima(y = sample.ts[, target])
+f_arima<-forecast(SPX_ARIMA, h=h)
+
+SPX_NN<-nnetar(sample.ts[, target])
+f_NN<-forecast(SPX_NN, h=h)
+
+# SPX Exogenas ------------------------------------------------------------
+
+SPX_ARIMAX<-auto.arima(y = sample.ts[, target], xreg =sample.ts[, exogena])
+yh<-forecast(SPX_ARIMAX, xreg = sample.ts.2[, exogena])
+
+SPX_NNX<-nnetar(y = sample.ts[, target],xreg =sample.ts[, exogena])
+y_nnx<-forecast(SPX_NNX, xreg = sample.ts.2[, exogena])
+
+compare_forecast<-ts.union(sample.ts.2[, target], yh$mean,f_arima$mean, f_NN$mean, y_nnx$mean, ts_f_spx$mean)
+colnames(compare_forecast)<-c('Observado','ARIMA-X' , 'ARIMA', 'NN', 'NN-X', 'TS_LM')
+
+compare_forecast<-data.frame(Fecha=seq.Date(from=as.Date('2018-02-01'), length.out = 11, by='month') ,
+                             compare_forecast, row.names = NULL)
+colnames(compare_forecast)[-1]<-c('Observado','ARIMA-X' , 'ARIMA', 'NN', 'NN-X','TS_LM')
+
+data_ggplot<-melt(compare_forecast, id='Fecha')
+
+library(ggplot2)
+
+ggplot(data_ggplot, aes(x=Fecha, y=value*100, col=variable))+
+  geom_line(size=1.5)+
+  scale_y_continuous(position = "right")+
+  scale_color_discrete(name='Metodos')+
+  ylab('')+xlab('')+ggtitle(paste0('Forecast ', target), 
+                            subtitle = '2018-02-2018-12')
+
+errores<-compare_forecast$Observado-compare_forecast[,-c(1,2)]
+media.errores<-colMeans(errores^2)
+rmse<-sqrt(media.errores)
+
+write.table( data.frame(rmse), "clipboard", sep="\t", na="") 
